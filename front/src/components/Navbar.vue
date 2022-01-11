f4ec7a7d00ada905e808f55c9b622fc98d20d698
Victoria Derudder <viki.derudder@gmail.com>
2022-01-11 16:16:11 -0300

    initialized vue project

---
+++ front/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand href="#">adoptame</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">Favoritos</b-nav-item>
+          <b-nav-item href="#">Donaciones</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-button variant="outline-primary">Ingresar</b-button>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
