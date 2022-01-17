26669399f818c2ca755057483995c675bdccd59e
Victoria Derudder <viki.derudder@gmail.com>
2022-01-17 15:10:28 -0300

    fix: deleted unnecessary components
    deleted FormularioLogin and FormularioSignup.
    added deleted content to correspondent view

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -11,19 +11,51 @@
         <h2>¡Bienvenido!</h2>
       </div>
 
-      <FormularioLogin />
+      <b-form>
+        <b-form-group
+          id="email-form"
+          label="Correo electrónico"
+          label-for="email"
+        >
+          <b-form-input
+            id="email-input"
+            type="email"
+            placeholder="ejemplo@ejemplo.com"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="password" label="Contraseña" label-for="contrasena">
+          <b-form-input id="password-form" required></b-form-input>
+        </b-form-group>
+
+        <div class="d-flex flex-column align-items-center">
+          <b-button type="submit" variant="primary" class="mb-2" block pill
+            >Iniciar sesión</b-button
+          >
+          <b-link href="#" class="mb-2 grey-link"
+            >¿Olvidó su contraseña?</b-link
+          >
+          <b-link to="signup">¿No tienes cuenta? Regístrate</b-link>
+        </div>
+
+        <div class="d-flex flex-column align-items-center mt-5">
+          <b-button type="submit" variant="secondary" class="mb-2" block pill
+            >Continuar con Facebook</b-button
+          >
+          <b-button type="submit" variant="secondary" class="mb-2" block pill
+            >Continuar con Google</b-button
+          >
+        </div>
+      </b-form>
     </b-container>
   </div>
 </template>
 
 <script>
-import FormularioLogin from "../components/FormularioLogin.vue";
 
 export default {
-  name: "Login",
-  components: {
-    FormularioLogin,
-  },
+
 };
 </script>
 
--- front/src/views/Signup.vue
+++ front/src/views/Signup.vue
@@ -10,18 +10,50 @@
         ></b-img>
         <h2>¡Bienvenido!</h2>
       </div>
-      <FormularioSignup />
+      <b-form>
+        <b-form-group
+          id="email-form"
+          label="Correo electrónico"
+          label-for="email"
+        >
+          <b-form-input
+            id="email-input"
+            type="email"
+            placeholder="ejemplo@ejemplo.com"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="password" label="Contraseña" label-for="contrasena">
+          <b-form-input id="password-form" required></b-form-input>
+        </b-form-group>
+
+        <div class="d-flex flex-column align-items-center">
+          <b-button type="submit" variant="primary" class="mb-4" block pill
+            >Registrarse</b-button
+          >
+          <p class="text-muted">O BIEN</p>
+        </div>
+
+        <div class="d-flex flex-column align-items-center mt-3">
+          <b-button type="submit" variant="secondary" class="mb-2" block pill
+            >Continuar con Facebook</b-button
+          >
+          <b-button type="submit" variant="secondary" class="mb-2" block pill
+            >Continuar con Google</b-button
+          >
+        </div>
+
+        <div class="text-center mt-5">
+          <b-link to="login">¿Ya tienes cuenta? Inicia sesión</b-link>
+        </div>
+      </b-form>
     </b-container>
   </div>
 </template>
 
 <script>
-import FormularioSignup from "../components/FormularioSignup.vue";
 export default {
-  name: "Signup",
-  components: {
-    FormularioSignup,
-  },
 };
 </script>
 
